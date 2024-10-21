45afbc0390d9a6eb53febb8a1e3c200e30ea1f83
Eduardo Vedes <eduardo.vedes@gmail.com>
2024-10-21 20:38:16 +0100

    feat: initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="grow flex flex-col gap-4 justify-center items-center">
+        <h1 className="text-3xl">eduardovedes.com</h1>
+        <div className="text-sm">eduardo.vedes@gmail.com</div>
+      </div>
+      <div className="h-12 text-xs flex justify-center items-center">
+        2024 &copy; All Rights Reserved
+      </div>
+    </div>
+  );
+}
