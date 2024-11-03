b2fc675e0578635c2581dde5a238659401d8675e
Eduardo Vedes <eduardo.vedes@gmail.com>
2024-11-03 23:32:48 +0000

    feat: clean page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="grow flex flex-col gap-4 justify-center items-center">
-        <h1 className="text-3xl">eduardovedes.com</h1>
-        <div className="text-sm">eduardo.vedes@gmail.com</div>
+        <Image
+          src="/profile-pic.png"
+          className="rounded-full shadow-xl"
+          alt="Eduardo Vedes"
+          width={180}
+          height={180}
+        />
+        <h1 className="text-4xl font-bold text-gray-800">Eduardo Vedes</h1>
+        <h1 className="text-2xl font-bold text-gray-800">eduardovedes.com</h1>
+        <div className="text-sm font-semibold">eduardo.vedes@gmail.com</div>
+        <div className="text-lg w-96 text-center p-4">
+          ❤️ Helping you to grow and thrive as a software engineer, 🌞 Solar
+          punk, 🦄 Indie hacker, 📒 Author of Code Your Futre, ☠️ Software
+          engineer
+        </div>
       </div>
       <div className="h-12 text-xs flex justify-center items-center">
         2024 &copy; All Rights Reserved
