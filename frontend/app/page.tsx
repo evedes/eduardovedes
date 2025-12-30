b7d505a3ad4a2c6752d993e3efa8a553dc112488
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-12-30 20:42:02 +0000

    feat: scroll on root page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import { getAllPosts } from "@/lib/blog";
 import { HomeClient } from "./components/home/HomeClient";
 
 export default function Home() {
-  const posts = getAllPosts().slice(0, 3); // Get latest 3 posts
+  const posts = getAllPosts().slice(0, 2); // Get latest 2 posts
 
   return <HomeClient posts={posts} />;
 }
