2da625a1d62a1c66af300df857c074a1d1596edf
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-12-30 17:39:44 +0000

    fix: next.config usage of mdx

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
-import rehypeHighlight from "rehype-highlight";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-const withMDX = createMDX({
-  options: {
-    rehypePlugins: [rehypeHighlight],
-  },
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
